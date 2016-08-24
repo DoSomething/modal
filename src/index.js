57c28b41a070a2379926dc06a1591a5298ff8210
David Furnes <david@dfurnes.com>
2016-08-24 13:40:23 -0400

    Use standard build system. Remove QUnit tests.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * This is the entry point for our Webpack build.
+ */
+
+// Import the SCSS file for base styles.
+import './modal.scss';
+
+// Now, import the library itself!
+import './modal';
+
